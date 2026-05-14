dc00021e9780d29d311a7e33e0c36bed7f70d0d1
Azam Gohar <144214140+azamgohar@users.noreply.github.com>
2026-05-14 21:15:41 +0500

    Add traffic signal system script

---
+++ raw/script.js
@@ -0,0 +1,15 @@
+// Traffic Signal System (TSS)
+
+let light = prompt("Pick anyone... red, yellow, green");
+
+switch (light) {
+    case "red":
+        console.log("Stop!");
+        break;
+    case "yellow":
+        console.log("Ready!");
+        break;
+    case "green":
+        console.log("Go!");
+        break;
+}
